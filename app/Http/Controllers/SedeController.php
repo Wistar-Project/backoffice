6a602f9a0678d423c947475cbb0ea6fa6da54930
fabrisdev <ezepuntocom@hotmail.com>
2023-11-08 18:31:07 -0300

    Ahora se puede crear sedes

--- app/Http/Controllers/SedeController.php
+++ app/Http/Controllers/SedeController.php
@@ -14,12 +14,6 @@ use Illuminate\Support\Facades\Validator;
 
 class SedeController extends Controller
 {
-    private function validarDatos($request){
-        return Validator::make($request -> all(), [
-            'direccion' => 'required|unique:alojamientos',
-            'tipo'=> 'required|in:sede,almacen'
-        ]);
-    }
 
     private function obtenerDatos($request){
         $direccion = $request -> post('direccion');
@@ -27,37 +21,21 @@ class SedeController extends Controller
         return [$direccion, $tipo];
     }
 
-    private function agregarAlojamiento($tipo, $direccion){
-        DB::transaction(function() use($tipo, $direccion){
-            $idAlojamiento = Alojamiento::create([
-                'direccion' => $direccion
-            ]) -> id;
-            if($tipo == 'almacen')
-                return Almacen::create([
-                    'id' => $idAlojamiento
-                ]);
-            Sede::create([
-                'id' => $idAlojamiento
-            ]);
+    private function agregarSede($direccion){
+        DB::transaction(function() use($direccion){
+            $idAlojamiento = Alojamiento::create(['direccion' => $direccion])->id;
+            Sede::create(['id' => $idAlojamiento]);
         });
     }
 
-    public function CrearAlojamiento(Request $request){
-        $validacion = $this -> validarDatos($request);
-        if($validacion -> fails())
-            return view('crearAlojamiento', [
-                "mensaje" => "Ha ocurrido un error. Por favor, revise los campos."
-            ]);
-        [ $direccion, $tipo ] = $this -> obtenerDatos($request);
-        $yaHayAlmacen = count(Almacen::all()) >= 1;
-        if($tipo == 'almacen' && $yaHayAlmacen)
-            return view('crearAlojamiento', [
-                "mensaje" => "No puedes crear mas de un almacén"
-            ]);
-        $this -> agregarAlojamiento($tipo, $direccion);
-        return view('crearAlojamiento',[
-            "mensaje" => "Alojamiento creado satisfactoriamente"
+    public function Crear(Request $request){
+        $validacion = Validator::make($request -> all(), [
+            'direccion' => 'required|unique:alojamientos|max:100|min:10',
         ]);
+        if($validacion -> fails())
+            return $this -> listarConError("Ya hay una sede con esa dirección.");
+        $this -> agregarSede($request -> input('direccion'));
+        return $this -> Listar();
     }
 
     public function Borrar(Request $request, $idSede){
