83a57ce1772837c09edfe6e40b91631bb63727b1
fabrisdev <ezepuntocom@hotmail.com>
2023-11-08 00:58:16 -0300

    Viejas rutas comentadas

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\AlojamientoController;
+use App\Http\Controllers\SedeController;
 use App\Http\Controllers\ConductorController;
 use App\Http\Controllers\LoteController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PersonaController;
 use App\Http\Controllers\PaqueteController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,58 +20,58 @@ use App\Http\Controllers\PaqueteController;
 
 Route::get('/', function () {
     return view('home');
-}) -> middleware('auth');
+})->middleware('auth');
 
-Route::get('/sedes', [AlojamientoController::class, "ListarAlojamientos"]);
+Route::get('/sedes', [SedeController::class, "ListarSedes"])->middleware('auth');
+/* viejas rutas
+Route::post('/listar', [PersonaController::class, "ListarPersona"])->middleware('auth');
+Route::get('/listar', [PersonaController::class, "ListarPersonas"])->middleware('auth');
 
-Route::post('/listar', [PersonaController::class, "ListarPersona"])-> middleware('auth');
-Route::get('/listar', [PersonaController::class, "ListarPersonas"])-> middleware('auth');
-
-Route::post('/crear', [PersonaController::class, "CrearPersona"])-> middleware('auth');
-Route::get('/crear', function() {
+Route::post('/crear', [PersonaController::class, "CrearPersona"])->middleware('auth');
+Route::get('/crear', function () {
     return view("crearUsuario");
-    
-})-> middleware('auth');
-Route::post('/editar', [PersonaController::class, "EditarPersona"])-> middleware('auth');
-Route::get('/editar', function() {
+
+})->middleware('auth');
+Route::post('/editar', [PersonaController::class, "EditarPersona"])->middleware('auth');
+Route::get('/editar', function () {
     return view("editarUsuario");
-})-> middleware('auth');
+})->middleware('auth');
 
-Route::get('/verLote', [LoteController::class, "VerLote"])-> middleware('auth'); 
+Route::get('/verLote', [LoteController::class, "VerLote"])->middleware('auth');
 
-Route::get('/editarLote', function() {
-        return view("editarLote");
-})-> middleware('auth');
-    
-Route::post('/agregarPaqueteALote', [LoteController::class, "AgregarPaqueteALote"])-> middleware('auth');
-Route::post('/removerPaqueteALote', [LoteController::class, "RemoverPaqueteALote"])-> middleware('auth');
+Route::get('/editarLote', function () {
+    return view("editarLote");
+})->middleware('auth');
 
-Route::post('/borrarLote',[LoteController::class, "BorrarLote"])-> middleware('auth');
-Route::get('/borrarLote',function(){
+Route::post('/agregarPaqueteALote', [LoteController::class, "AgregarPaqueteALote"])->middleware('auth');
+Route::post('/removerPaqueteALote', [LoteController::class, "RemoverPaqueteALote"])->middleware('auth');
+
+Route::post('/borrarLote', [LoteController::class, "BorrarLote"])->middleware('auth');
+Route::get('/borrarLote', function () {
     return view('borrarLote');
-})-> middleware('auth');
+})->middleware('auth');
 
-Route::get('/listarPaquetes', [PaqueteController::class,"ListarPaquetes"])-> middleware('auth');
+Route::get('/listarPaquetes', [PaqueteController::class, "ListarPaquetes"])->middleware('auth');
 
-Route::post('/borrarPaquete',[PaqueteController::class, "BorrarPaquete"])-> middleware('auth');
-Route::get('/borrarPaquete',function(){
+Route::post('/borrarPaquete', [PaqueteController::class, "BorrarPaquete"])->middleware('auth');
+Route::get('/borrarPaquete', function () {
     return view('borrarPaquete');
-})-> middleware('auth');
-Route::post('/crearAlojamiento',[AlojamientoController::class, "CrearAlojamiento"])-> middleware('auth');
-Route::get('/crearAlojamiento',function(){
+})->middleware('auth');
+Route::post('/crearAlojamiento', [AlojamientoController::class, "CrearAlojamiento"])->middleware('auth');
+Route::get('/crearAlojamiento', function () {
     return view('crearAlojamiento');
-})-> middleware('auth');
+})->middleware('auth');
 
-Route::post('/borrarAlojamiento',[AlojamientoController::class, "BorrarAlojamiento"])-> middleware('auth');
-Route::get('/borrarAlojamiento',function(){
+Route::post('/borrarAlojamiento', [AlojamientoController::class, "BorrarAlojamiento"])->middleware('auth');
+Route::get('/borrarAlojamiento', function () {
     return view('borrarAlojamiento');
-})-> middleware('auth');
+})->middleware('auth');
 
-Route::get('/listarAlojamientos',[AlojamientoController::class, "ListarAlojamientos"])-> middleware('auth');
+Route::get('/listarAlojamientos', [AlojamientoController::class, "ListarSedes"])->middleware('auth');
 
-Route::get('/asignarConductor', function(){
+Route::get('/asignarConductor', function () {
     return view("asignarConductor");
-})-> middleware('auth');
-Route::post('/asignarConductor', [ ConductorController::class, "AsignarVehiculo" ])-> middleware('auth');
-
-Route::get('/logout', [ PersonaController::class, "CerrarSesion" ])-> middleware('auth');
+})->middleware('auth');
+Route::post('/asignarConductor', [ConductorController::class, "AsignarVehiculo"])->middleware('auth');
+*/
+Route::get('/logout', [PersonaController::class, "CerrarSesion"])->middleware('auth');
