1e7992d62b9c38ab21bc6b4383b87d9c0c98286c
fabrisdev <ezepuntocom@hotmail.com>
2023-11-08 01:04:20 -0300

    Ahora muestra las sedes

--- resources/views/sedes.blade.php
+++ resources/views/sedes.blade.php
@@ -2,26 +2,16 @@
 <link rel="stylesheet" href="styles/sedes.css">
 <h2 class="titulo">Sedes</h2>
 <main>
-    <a href="javascript:history.back()" class="volver" id="volver"> 
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
-            <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
-          </svg>
-        Volver</a>
+    @include('components/boton-volver')
     <button type="button" id="boton-buscar">Crear</button>
     <fieldset>
         <legend>Sedes</legend>
-        <div class="sede-container">
-            <button></button>
-            <p></p>
-        </div>
-        <div class="sede-container">
-            <button></button>
-            <p></p>
-        </div>
-        <div class="sede-container">
-            <button></button>
-            <p></p>
-        </div>
+        @foreach($sedes as $sede)
+            <div class="sede-container">
+                <button></button>
+                <p>{{ $sede -> alojamiento -> direccion }}</p>
+            </div>
+        @endforeach
     </fieldset>
 </main>
 @include('components/footer')
