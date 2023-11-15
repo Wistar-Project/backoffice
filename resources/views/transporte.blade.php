0169a20dde039c7ecfc67ecfd01e2b7191918c12
fabrisdev <ezepuntocom@hotmail.com>
2023-11-14 21:19:29 -0300

    Inicio de la view de transporte

---
+++ resources/views/transporte.blade.php
@@ -0,0 +1,28 @@
+<x-header/>
+<link rel="stylesheet" href="/styles/sedes.css">
+<script src="/js/sedes.js" defer></script>
+
+<h2 class="titulo">Sedes</h2>
+@isset($mensaje)
+    <x-alerta :color="$mensaje['color']">{{ $mensaje['texto'] }}</x-alerta>
+@endisset
+<main>
+    <x-boton-volver/>
+    <button type="button" id="boton-crear">Crear</button>
+    <form id="creacion-popup" method="POST" action="/sedes">
+        @csrf
+        <div class="titulo-crear">
+            <p>Crear sede</p>
+            <img src="/img/cruz.png" alt="" width="20px" height="20px" id="cerrar-creacion">
+        </div>
+        <div class="inputs">
+            <label for="direccion">Dirección:</label>
+            <input type="text" name="direccion" id="direccion" minlength="10" maxlength="100" required>
+        </div>
+        <input type="submit" value="Crear">
+    </form>
+    <fieldset>
+        <legend>Sedes</legend>
+    </fieldset>
+</main>
+<x-footer/>
