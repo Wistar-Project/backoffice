84c98d4da0857c6b1ef254701ef8bf5a3960d6f6
FranciscoCazeneuve <franciscocazeneuve02@gmail.com>
2023-11-15 07:06:03 -0300

    se le asignó ID a los textos en home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,25 @@
 @include('components/header')
-<h3 id="aplicaciones-titulo" data-text-id="400">Administración</h3>
+<h3 id="aplicaciones-titulo" data-text-id="407">Administración</h3>
 <main>
     <a class="container" id="usuarios-container" href="/usuarios">
-        <h3 data-text-id="401">Usuarios</h3>
+        <h3 data-text-id="463">Usuarios</h3>
         <img src="img/usuarios.png" alt="Hubo un error al cargar la imagen.">
-        <p class="texto-app" data-text-id="402">Registra nuevos usuarios en el sistema, listalos y elimina o edita otros.</p>
+        <p class="texto-app" data-text-id="464">Registra nuevos usuarios en el sistema, listalos y elimina o edita otros.</p>
     </a>
     <a class="container" id="transporte-container" href="/transporte">
-        <h3 data-text-id="403">Transporte</h3>
+        <h3 data-text-id="465">Transporte</h3>
         <img src="img/camion 2.png" alt="Hubo un error al cargar la imagen.">
-        <p class="texto-app" data-text-id="404">Define los vehículos que posees y asigna choferes a cargo de ellos.</p>
+        <p class="texto-app" data-text-id="466">Define los vehículos que posees y asigna choferes a cargo de ellos.</p>
     </a>
     <a class="container" id="paqueteria-container" href="/paqueteria">
-        <h3 data-text-id="405">Paquetería</h3>
+        <h3 data-text-id="467">Paquetería</h3>
         <img src="img/caja-del-paquete.png" alt="Hubo un error al cargar la imagen.">
-        <p class="texto-app" data-text-id="406">Lista paquetes o lotes, edita los que necesites o elimina los que desees.</p>
+        <p class="texto-app" data-text-id="468">Lista paquetes o lotes, edita los que necesites o elimina los que desees.</p>
     </a>
     <a class="container" id="sedes-container" href="/sedes">
-        <h3 data-text-id="407">Sedes</h3>
+        <h3 data-text-id="469">Sedes</h3>
         <img src="img/administración_blanco 1.png" alt="Hubo un error al cargar la imagen.">
-        <p class="texto-app" data-text-id="408">Define todas las sedes de la empresa para poder asignar camiones hacia esa dirección.</p>
+        <p class="texto-app" data-text-id="470">Define todas las sedes de la empresa para poder asignar camiones hacia esa dirección.</p>
     </a>
 </main>
 @include('components/footer')
