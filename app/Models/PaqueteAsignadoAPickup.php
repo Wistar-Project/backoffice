13a287bcd5420bb28b7c376fd0a082162ad2021c
fabrisdev <ezepuntocom@hotmail.com>
2023-11-15 02:11:25 -0300

    Nuevos modelos necesarios

---
+++ app/Models/PaqueteAsignadoAPickup.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaqueteAsignadoAPickup extends Model
+{
+    use HasFactory;
+    protected $table = "paquete_asignado_a_pickup";
+}
