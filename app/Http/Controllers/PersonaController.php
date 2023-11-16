8c6fb0b5d764333c1867d972c6ee8e13495f6479
fabrisdev <ezepuntocom@hotmail.com>
2023-11-15 21:38:05 -0300

    Comprobar funcionamiento de User

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -194,8 +194,8 @@ class PersonaController extends Controller
             return $this-> listarConError("No puedes eliminar un usuario gerente");
         $excepcion = DB::transaction(function() use($id){ 
             $this->borrarRol($id);
-            Persona::find($id)->delete();
-            User::findOrFail($id)->delete();
+            Persona::findOrFail($id)->delete();
+            User::find($id)->delete();
         });
         if(!is_null($excepcion))
             return $this-> ListarConmensaje("Ha ocurrido un error al intentar borrar el usuario.");
