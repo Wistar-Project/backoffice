944427a129a4d591ccd8c83a7a7c45adbb582945
fabrisdev <ezepuntocom@hotmail.com>
2023-11-15 21:32:17 -0300

    Comprobar funcionamiento de PersonaRol

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -189,7 +189,7 @@ class PersonaController extends Controller
             Conductor::findOrFail($id)->delete();
     }
     public function BorrarUsuario($id){
-        $rol = PersonaRol::findOrFail($id)->rol;
+        $rol = PersonaRol::find($id)->rol;
         if($rol == 'gerente')
             return $this-> listarConError("No puedes eliminar un usuario gerente");
         $excepcion = DB::transaction(function() use($id){ 
